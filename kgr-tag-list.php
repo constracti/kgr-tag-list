074be2e4be426c4bd17d351971da63f02331d42d
constracti <constracti@gmail.com>
2017-01-18 17:18:45 +0200

    add plugin uri

--- kgr-tag-list.php
+++ kgr-tag-list.php
@@ -1,11 +1,13 @@
 <?php
 /*
  * Plugin Name: KGR Tag List
+ * Plugin URI: https://github.com/constracti/kgr-tag-list
  * Description: Displays a widget with a list of links to selected tags, optionally filtered by a category.
  * Author: constracti
  * Version: 1.1
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /languages
  */
 
 if ( !defined( 'ABSPATH' ) )
